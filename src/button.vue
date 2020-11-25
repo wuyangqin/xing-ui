be7bfa77d89802e2264a5852cc02e1a9bbda9fcf
e <shiokoy1209@gmail.com>
2020-11-25 23:28:42 +0800

    验证button的图标位置所传值为left或right

--- src/button.vue
+++ src/button.vue
@@ -19,7 +19,10 @@ export default {
     },
     iconPosition: {
       type: String,
-      default: 'left'
+      default: 'left',
+      validator (value) {
+        return value === 'left' || value === 'right'
+      }
     }
   },
   data() {
