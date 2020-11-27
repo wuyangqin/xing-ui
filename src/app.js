83c94e2171eb5e3a3f3a1101ecfa1790da734dba
e <shiokoy1209@gmail.com>
2020-11-27 21:23:14 +0800

    添加button-group

--- src/app.js
+++ src/app.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import  XButton from './button'
 import  XIcon from './icon'
+import  ButtonGroup from './button-group'
 
 Vue.component('x-button', XButton)
+Vue.component('x-button-group', ButtonGroup)
 Vue.component('x-icon', XIcon)
 
 new Vue ({
---
+++ src/button-group.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="x-button-group">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {
+  }
+}
+</script>
+
+<style lang="less">
+.x-button-group {
+  display: inline-flex;
+  vertical-align: middle;
+  //border-radius: var(--border-radius);
+  //overflow: hidden;
+  .x-button {
+    border-radius: 0;
+    border-right: 1px solid rgba(255, 255, 255, .5);
+    &:first-child {
+      border-radius: var(--border-radius) 0 0 var(--border-radius);
+    }
+    &:last-child {
+      border-right: none;
+      border-radius: 0 var(--border-radius) var(--border-radius) 0;
+    }
+  }
+}
+</style>
