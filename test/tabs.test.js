fa54dbd879dac973be5cce27a41ac3e6739802b6
e <shiokoy1209@gmail.com>
2021-01-22 12:04:21 +0800

    tab测试用例

--- test/tabs.test.js
+++ test/tabs.test.js
@@ -1,21 +1,25 @@
-const expect = chai.expect;
 import Vue from 'vue'
 import Tabs from '../src/tabs/tabs'
 import Tab from '../src/tabs/tab'
+
+const expect = chai.expect
 Vue.component('x-tabs', Tabs)
 Vue.component('x-tab', Tab)
 
 Vue.config.productionTip = false
 Vue.config.devtools = false
 
-describe ('Tabs', () => {
-  it ('Tabs存在.', () => {
+describe('Tabs和tab', () => {
+  it('Tabs存在.', () => {
     expect(Tabs).to.be.ok
   })
-  describe ('props', () => {
+  it('Tab存在.', () => {
+    expect(Tab).to.be.ok
+  })
+  describe('tabsProps', () => {
     const Constructor = Vue.extend(Tabs)
     let vm
-    it ('可以接收value', (done) => {
+    it('可以接收value', (done) => {
       const div = document.createElement('div')
       document.body.appendChild(div)
       div.innerHTML = `
@@ -36,7 +40,7 @@ describe ('Tabs', () => {
         done()
       })
     })
-    it ('可以接收tabPosition', () => {
+    it('可以接收tabPosition', () => {
       vm = new Constructor({
         propsData: {
           value: '1',
@@ -47,8 +51,74 @@ describe ('Tabs', () => {
       vm.$destroy()
     })
   })
-  describe ('events', () => {
-    it ('切换事件change被触发', (done) => {
+  describe('tabProps', () => {
+    const Constructor = Vue.extend(Tab)
+    let vm
+    it('可以接收label', (done) => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      div.innerHTML = `
+        <x-tabs value="kaixin">
+          <x-tab label="kaixin" name="kaixin"></x-tab>
+        </x-tabs>
+      `
+      const vm = new Vue({
+        el: div
+      })
+      setTimeout(() => {
+        const tabNav = vm.$el.querySelector('.x-tab-nav__item')
+        expect(tabNav.innerHTML).to.eq('kaixin')
+        vm.$el.remove()
+        vm.$destroy()
+        done()
+      })
+    })
+    it('可以接收name', (done) => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      div.innerHTML = `
+        <x-tabs ref="tabs" value="kaixin">
+          <x-tab label="kaixin" name="kaixin"></x-tab>
+        </x-tabs>
+      `
+      const vm = new Vue({
+        el: div
+      })
+      setTimeout(() => {
+        const tabNav = vm.$refs.tabs.$refs.tabNav.$refs['tab-kaixin']
+        expect(tabNav.classList.contains('x-tab-nav__item-kaixin')).to.eq(true)
+        vm.$el.remove()
+        vm.$destroy()
+        done()
+      })
+    })
+    it('可以接收disabled', (done) => {
+      const div = document.createElement('div')
+      document.body.appendChild(div)
+      div.innerHTML = `
+        <x-tabs ref="tabs" value="kaixin">
+          <x-tab label="kaixin" name="kaixin" disabled></x-tab>
+        </x-tabs>
+      `
+      const vm = new Vue({
+        el: div
+      })
+      setTimeout(() => {
+        const tabNav = vm.$el.querySelector('.x-tab-nav__item')
+        expect(tabNav.classList.contains('nav-disabled')).to.eq(true)
+        const tabsNode = vm.$refs.tabs
+        const callback = sinon.fake()
+        tabsNode.$on('change', callback)
+        tabNav.click()
+        expect(callback).to.have.not.been.called
+        vm.$el.remove()
+        vm.$destroy()
+        done()
+      })
+    })
+  })
+  describe('events', () => {
+    it('切换事件change被触发', (done) => {
       const div = document.createElement('div')
       document.body.appendChild(div)
       div.innerHTML = `
