474adb2ef2facfaedc4de38595e5e5f624782881
yang <shiokoy1209@gmail.com>
2021-04-09 17:08:14 +0800

    [cascader]导入测试数据

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
   <div id="app">
     <cascader v-model="selected" :source="source" @change="change">
-<!--      <button>点我</button>-->
+      <!--      <button>点我</button>-->
     </cascader>
   </div>
 </template>
 
 <script>
 import Cascader from './components/cascader/index'
+import db from './components/cascader/db.js'
 
+const ajax = function (id = 0) {
+  return new Promise((resolve, reject) => {
+    let result = []
+    db.forEach(item => {
+      if (item.parent_id === id) {
+        const {id, parent_id, name, value} = item
+        result.push({id, parent_id, name, value})
+      }
+    })
+    resolve(result)
+  })
+}
 export default {
   name: "App",
   components: {
@@ -18,61 +31,66 @@ export default {
     return {
       visible: false,
       // selected:['zhejiang','hangzhou','xiacheng'],
-      selected:[],
+      selected: [],
       source: [{
         name: '浙江',
-        value:'zhejiang',
+        value: 'zhejiang',
         children: [
           {
             name: '杭州',
-            value:'hangzhou',
+            value: 'hangzhou',
             children: [
-              {name: '上城',value:'shangcheng'},
-              {name: '下城',value:'xiacheng'},
-              {name: '江干',value:'jianggan'},
+              {name: '上城', value: 'shangcheng'},
+              {name: '下城', value: 'xiacheng'},
+              {name: '江干', value: 'jianggan'},
             ]
           },
           {
             name: '嘉兴',
-            value:'anhui',
+            value: 'anhui',
             children: [
-              {name: '南湖',value:'nanhu'},
-              {name: '秀洲',value:'xiuzhou'},
-              {name: '嘉善',value:'jiashan'},
+              {name: '南湖', value: 'nanhu'},
+              {name: '秀洲', value: 'xiuzhou'},
+              {name: '嘉善', value: 'jiashan'},
             ]
           },
         ]
       }, {
         name: '福建',
-        value:'fujian',
+        value: 'fujian',
         children: [
           {
             name: '福州',
-            value:'fuzhou',
+            value: 'fuzhou',
             children: [
-              {name: '鼓楼',value:'gulou'},
-              {name: '台江',value:'taijiang'},
-              {name: '仓山',value:'cangshan'},
+              {name: '鼓楼', value: 'gulou'},
+              {name: '台江', value: 'taijiang'},
+              {name: '仓山', value: 'cangshan'},
             ]
           },
         ]
       }, {
         name: '安徽',
-        value:'anhui',
+        value: 'anhui',
         children: [{
           name: '合肥',
-          value:'hefei',
+          value: 'hefei',
           children: [{
-            name: '瑶海',value:'yaohai'
+            name: '瑶海', value: 'yaohai'
           }, {
-            name: '庐阳',value:'luyang'
+            name: '庐阳', value: 'luyang'
           }]
         }]
       }]
     }
   },
+  created() {
+    ajax(0).then(result => {
+      console.log(result);
+    })
+  },
   methods: {
-    change(e){
+    change(e) {
       console.log(e);
     }
   }
