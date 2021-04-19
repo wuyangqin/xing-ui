c22f129abe9c0ff236c8068b10efe1a8f46b2488
yang <shiokoy1209@gmail.com>
2021-04-19 14:51:45 +0800

    [打包发布]使用vue cli的lib模式打包

--- index.js
+++
@@ -1,5 +0,0 @@
-import  XButton from './src/components/button'
-import XButtonGroup from './src/components/button-group'
-import XIcon from './src/components/icon'
-
-export { XButton, XButtonGroup, XIcon }
